ed4658dcacbc8024a91c78e31cfea37b82c419c2
Michal <michalsorrensen@gmail.com>
2020-07-30 19:44:26 +0200

    flytter

---
+++ index.js
@@ -0,0 +1,41 @@
+// index.js
+
+/**
+ * Required External Modules
+ */
+const express = require("express");
+const path = require("path");
+
+/**
+ * App Variables
+ */
+const app = express();
+const port = process.env.PORT || "8000";
+
+
+/**
+ *  App Configuration
+ */
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+/**
+ * Routes Definitions
+ */
+app.get("/", (req, res) => {
+    res.render("index", { title: "Home" });
+  });
+
+ 
+ 
+// kan ikke få den anden side til at fungere. 
+  app.get('/user', (req, res) => {
+    res.send(req.query.id);
+  });
+
+ /** Server Activation
+ */
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+  });
